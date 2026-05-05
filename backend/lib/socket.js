58dacb95cb0547a8a9a686231adea1d24d953fea
iamesssam <essamali19@icloud.com>
2026-05-05 18:35:59 +0300

    Update socket.js

--- backend/lib/socket.js
+++ backend/lib/socket.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5174",
+        origin: "https://chatify-frontend-6n9v.onrender.com",
         credentials: true
     }
 })
