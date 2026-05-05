545176b5246ed8a28d92f7ec6da6b06e348ab44b
iamesssam <essamali19@icloud.com>
2026-05-05 18:05:22 +0300

    Update MessageInput.jsx

--- frontend/src/components/MessageInput.jsx
+++ frontend/src/components/MessageInput.jsx
@@ -12,24 +12,6 @@ const MessageInput = () => {
 
     const { sendMessages } = useChatStore();
 
-    // const handleSendMessage = (e) => {
-    //     e.preventDefault();
-    //     if (!text.trim() && !image) return;
-
-    //     const formData = new FormData();
-    //     formData.append('image', image);
-
-    //     sendMessages({
-    //         text: text.trim(),
-    //         image: image
-    //     })
-    //     setText("");
-    //     setImage(false);
-
-    //     if (fileInputRef.current) fileInputRef.current.value = "";
-    // }
-
-
     const handleSendMessage = (e) => {
         e.preventDefault();
         if (!text.trim() && !image) return;
@@ -70,98 +52,6 @@ const MessageInput = () => {
         setImagePreview(null);
         if (fileInputRef.current) fileInputRef.current.value = "";
     }
-
-    // return (
-    //     <div className='p-4 border-t border-slate-700/50'>
-    //         {image &&
-    //             (
-    //                 <div className="max-w-3xl mx-auto mb-3 flex items-center">
-    //                     <div className="relative">
-    //                         <img
-    //                             src={imagePreview}
-    //                             alt="Preview"
-    //                             className="w-20 h-20 object-cover rounded-lg border border-slate-700"
-    //                         />
-    //                         <button
-    //                             onClick={removeImage}
-    //                             className="absolute -top-2 -right-2 w-6 h-6 rounded-full bg-slate-800 flex items-center justify-center
-    //                              text-slate-200 hover:bg-slate-700 cursor-pointer"
-    //                             type="button"
-    //                         >
-    //                             <XIcon className="w-4 h-4 " />
-    //                         </button>
-    //                     </div>
-    //                 </div>
-    //             )}
-
-
-    //         <form onSubmit={handleSendMessage} className='max-w-3xl mx-auto flex space-x-4
-    //         '>
-    //             <input
-    //                 value={text}
-    //                 onChange={(e) => setText(e.target.value)}
-    //                 className='flex-1 bg-slate-800/50 border border-slate-700/50 rounded-lg py-3 px-4
-    //                 placeholder:text-slate-400
-    //                 text-slate-200 font-bold
-    //                 '
-    //                 placeholder='Type your message...'
-    //             />
-
-    //             <input type="file"
-    //                 accept='image/*'
-    //                 ref={fileInputRef}
-    //                 onChange={handleImageChange}
-    //                 // onChange={(e) => setImage(e.target.files[0])}
-    //                 className='hidden'
-    //             />
-
-    //             <button type='button'
-    //                 onClick={() => fileInputRef.current?.click()}
-    //                 className={`bg-slate-800/50 text-slate-400 hover:text-slate-200 
-    //                     rounded-lg px-4 transition-colors ${image ? "text-cyan-500" : ""
-    //                     }
-    //                 !cursor-pointer
-    //                     `}
-    //             >
-    //                 <ImageIcon />
-
-    //             </button>
-    //             {/* {
-    //                 text ?
-    //                     <button
-    //                         type="submit"
-    //                         // disabled={!text.trim() && !imagePreview}
-    //                         className="bg-gradient-to-r from-cyan-500 to-cyan-600 text-white rounded-lg px-4 py-2 font-medium hover:from-cyan-600 hover:to-cyan-700 transition-all 
-    //                     disabled:opacity-50 disabled:cursor-not-allowed cursor-pointer"
-    //                     >
-    //                         <SendIcon className="w-5 h-5" />
-    //                     </button> : ""
-    //             } */}
-
-    //             <button
-    //                 type="submit"
-    //                 disabled={!text.trim() && !imagePreview} // هيفضل معطل لو مفيش داتا بس موجود
-    //                 className={`
-    //     /* المساحات والأبعاد الثابتة */
-    //     shrink-0 w-12 h-12 flex items-center justify-center rounded-xl ml-2
-
-
-    //     bg-gradient-to-r from-cyan-500 to-cyan-600 text-white
-    //     shadow-lg shadow-cyan-900/20
-    //     hover:from-cyan-600 hover:to-cyan-700 
-    //     active:scale-95 transition-all duration-200
-
-
-    //     disabled:opacity-40 disabled:grayscale disabled:cursor-not-allowed
-    //     !cursor-pointer
-    // `}
-    //             >
-    //                 <SendIcon className="w-5 h-5 md:w-6 md:h-6" />
-    //             </button>
-
-    //         </form>
-    //     </div >
-    // )
 
     return (
         <div className='p-2 md:p-4 border-t border-slate-700/50 bg-base-100'>
